195c212b9fa6e3f199363cd99ae091e724430720
madhavan-raja <madhavanraja99@gmail.com>
2022-03-09 22:10:00 +0530

    Enabled evil-org everywhere

--- init.el
+++ init.el
@@ -171,10 +171,15 @@
   (evil-collection-init))
 
 (use-package evil-org
+  :diminish evil-org-mode
   :after org
   :config
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda () (evil-org-set-key-theme))))
+
+(require 'evil-org-agenda)
+(evil-org-agenda-set-keys)
 
 (use-package evil-leader
   :config
